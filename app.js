786c465019ef8bb934510f41b875d36903a5cc26
ericndungutse <dav.ndungutse@gmil.com>
2024-01-24 14:34:52 +0200

    configure cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 
 import userRouter from './routes/user.routes.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
