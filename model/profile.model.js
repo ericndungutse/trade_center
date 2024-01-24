7b6821b4eb640e0bc17d06ec3d213e9724f72817
ericndungutse <dav.ndungutse@gmil.com>
2024-01-24 10:09:52 +0200

    create company profile

---
+++ model/profile.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+const companySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+
+    type: {
+      type: String,
+      enum: ['manufacturing', 'finance', 'technology'],
+      required: true,
+      lowercase: true,
+    },
+
+    companyLogo: {
+      type: String,
+    },
+
+    location: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      // Array of coordinates, Longitude First, latitude last
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+    versionKey: false,
+  }
+);
+
+const Company = mongoose.model('Company', companySchema);
+export default Company;
