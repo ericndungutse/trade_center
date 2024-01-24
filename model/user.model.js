a9fe31d6d4687db93b844a232805ea205d9310fc
ericndungutse <dav.ndungutse@gmil.com>
2024-01-24 09:26:51 +0200

    create user model

---
+++ model/user.model.js
@@ -0,0 +1,59 @@
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name.'],
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique: true,
+    lowercase: true,
+  },
+
+  photo: { type: String, default: 'default.jpg' },
+
+  role: {
+    type: String,
+    enum: ['systemadmin', 'admin', 'user'],
+    default: 'user',
+  },
+
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+    select: false,
+  },
+
+  active: {
+    type: String,
+    default: true,
+    select: false,
+  },
+
+  verified: {
+    type: String,
+    default: true,
+    select: false,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  // CHECK IF PASSWORD WAS MODIFIED
+  // IF NO, Return AND GO OVER
+  if (!this.isModified('password')) return next();
+
+  // IF YES HASH THE PASSWORD
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
